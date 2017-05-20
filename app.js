17487f8faa37efb398911797cd4725b7c00a4cca
Jordan Knight <jordanknight@Jordans-MacBook-Pro.local>
2017-05-20 10:38:13 -0500

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+ var config = {
+   apiKey: “AIzaSyCY0F96DgCztY6rWJLHdigKNPAYZt4pbNQ”,
+   authDomain: “employee-timesheet-bbcd7.firebaseapp.com”,
+   databaseURL: “https://employee-timesheet-bbcd7.firebaseio.com“,
+   projectId: “employee-timesheet-bbcd7”,
+   storageBucket: “employee-timesheet-bbcd7.appspot.com”,
+   messagingSenderId: “710286577551”
+ };
+ firebase.initializeApp(config);
