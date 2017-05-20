7bdde3f4e8830338c584a9ba910c5817c38dd32d
Jordan Knight <jordanknight@Jordans-MacBook-Pro.local>
2017-05-20 11:02:48 -0500

    firebase push

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+ $(document).ready(function(){
 // Initialize Firebase
- var config = {
-   apiKey: “AIzaSyCY0F96DgCztY6rWJLHdigKNPAYZt4pbNQ”,
-   authDomain: “employee-timesheet-bbcd7.firebaseapp.com”,
-   databaseURL: “https://employee-timesheet-bbcd7.firebaseio.com“,
-   projectId: “employee-timesheet-bbcd7”,
-   storageBucket: “employee-timesheet-bbcd7.appspot.com”,
-   messagingSenderId: “710286577551”
- };
- firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyCY0F96DgCztY6rWJLHdigKNPAYZt4pbNQ",
+    authDomain: "employee-timesheet-bbcd7.firebaseapp.com",
+    databaseURL: "https://employee-timesheet-bbcd7.firebaseio.com",
+    projectId: "employee-timesheet-bbcd7",
+    storageBucket: "employee-timesheet-bbcd7.appspot.com",
+    messagingSenderId: "710286577551"
+  };
+  firebase.initializeApp(config);
+ 
+ var database = firebase.database();
+
+
+
+ $("#addEmp").on("click", function(event){
+
+ 	event.preventDefault();
+
+ 	var name = $("#name").val().trim();
+ 	var role = $("#role").val().trim();
+ 	var sDate = $("#sDate").val().trim();
+ 	var rate = $("#rate").val().trim();
+
+ 	database.ref().push({
+ 		name : name,
+ 		role : role,
+ 		startDate : sDate,
+ 		rate : rate
+
+ 	});
+
+
+
+ });
+});
